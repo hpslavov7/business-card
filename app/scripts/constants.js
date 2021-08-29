80ec0b65e74aa7873e0dc412b03c550018905f36
hpslavov7 <hpslavov7@gmail.com>
2021-08-29 15:08:14 +0300

    Added jquery animations. Added structure for desktop/mobile development. Added initial design logic for the desktop implementation.

---
+++ app/scripts/constants.js
@@ -0,0 +1,7 @@
+var Constants = {
+    landingImageMaxHeight: 92,
+    landingImageMinHeight: 0,
+    autoHeight: 'auto',
+    displayNone: 'none',
+    displayBlock: 'block'
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('here');
+var lorem = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.';
+
+var isMobileDevice = navigator.userAgentData.mobile;
+
+console.log(isMobileDevice);
+
+
+if (isMobileDevice) {
+    MobileDevice.run();
+} else {
+    DesktopDevice.run();
+}
